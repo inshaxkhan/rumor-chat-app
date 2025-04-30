cfe8061911250580d39f4fda1ac9887499f0ea32
Insha Khan <khaninsha0001@gmail.com>
2025-05-01 02:05:45 +0530

    initial commit for chat app

---
+++ src/assets/assets.js
@@ -0,0 +1,51 @@
+import avatar_icon from './avatar_icon.jpeg'
+import gallery_icon from './gallery_icon.jpeg'
+import help_icon from './help_icon.jpeg'
+import logo_icon from './logo_icon.png'
+import logo_big from './logo_big.png'
+import logo from './logo.png'
+import img1 from './img1.jpeg'
+import img2 from './img2.jpeg'
+import img3 from './img3.jpeg'
+import img4 from './img4.jpeg'
+import img5 from './img5.jpeg'
+import img6 from './img6.jpeg'
+import profile_ana from './profile_ana.jpeg'
+import profile_brian from './profile_brian.jpeg'
+import profile_celia from './profile_celia.jpeg'
+import profile_demon from './profile_demon.jpeg'
+import profile_elsa from './profile_elsa.jpeg'
+import search_icon from './search_icon.jpeg'
+import send_icon from './send_icon.jpeg'
+import add_icon from './add_icon.jpeg'
+import menu_icon from './menu_icon.jpeg'
+import arrow_icon from './arrow_icon.jpeg'
+import green_dot from './green_dot.jpeg'
+
+const assets = {
+    avatar_icon,
+    gallery_icon,
+    help_icon,
+    logo_big,
+    logo_icon,
+    logo,
+    img1,
+    img2,
+    img3,
+    img4,
+    img5,
+    img6,
+    profile_ana,
+    profile_brian,
+    profile_celia,
+    profile_elsa,
+    profile_demon,
+    search_icon,
+    send_icon,
+    add_icon,
+    menu_icon,
+    arrow_icon,
+    green_dot
+}
+
+export default assets;
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Login.css'
+import assets from '../../assets/assets.js'
+
+const Login = () => {
+  return (
+    <div className='login'>
+      <img src={assets.logo_big} alt="" className="logo"/>
+    </div>
+  )
+}
+
+export default Login
