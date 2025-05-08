77e58b987b8decd2df8e575c89ae236b0b4e8102
Insha Khan <khaninsha0001@gmail.com>
2025-05-08 15:52:28 +0530

    feat: add profile update with image upload, Firebase sync, and context integration

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,20 +1,54 @@
+import { doc, getDoc, updateDoc } from "firebase/firestore";
 import { createContext, useState } from "react";
+import { auth, db } from "../config/firebase";
+import { useNavigate } from "react-router-dom";
 
-export const AppContext = createContext();
+export const AppContext=createContext();
 
-const AppContextProvider = (props) => {
-    const [userData,setUserData]=useState(null);
-    const [chatData,setChatData]=useState(null);
+const AppContextProvider=(props)=>{
+    const navigate=useNavigate();
+    const [userData, setUserData]=useState(null);
+    const [chatData, setChatData]=useState(null);
 
-    const value = {
-        userData,setUserData,
-        chatData,setChatData
+    const loadUserData=async(uid)=>{
+        try {
+            const userRef=doc(db,"users",uid);
+            const userSnap=await getDoc(userRef);
+            const userData=userSnap.data();
+            setUserData(userData);
+            if(userData.avatar && userData.name){
+                navigate('/chat');
+            }
+            else{
+                navigate('/profile')
+            }
+
+            await updateDoc(userRef,{
+                lastSeen:Data.now()
+            })
+
+            setInterval(async() => {
+                if(auth.chatUser){
+                    await updateDoc(userRef,{
+                        lastSeen:Date.now()
+                    })
+                }
+            }, 60000);
+        } catch (error) {
+            
+        }
     }
 
-    return (
-        <AppContextProvider value={value}>
+    const value={
+        userData, setUserData,
+        chatData, setChatData,
+        loadUserData
+    }
+
+    return ( 
+        <AppContext.Provider value={value}>
             {props.children}
-        </AppContextProvider>
+        </AppContext.Provider>
     )
 }
 
--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,26 +1,102 @@
-import React, { useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './ProfileUpdate.css'
 import './../../assets/assets.js'
 import assets from './../../assets/assets.js'
+import { onAuthStateChanged } from 'firebase/auth'
+import { doc, getDoc, updateDoc } from 'firebase/firestore'
+import { auth, db } from '../../config/firebase.js'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import upload from '../../lib/upload.js'
+import { AppContext } from '../../context/AppContext.jsx'
 
 const ProfileUpdate = () => {
-  const [image, setImage]=useState(false);
+  const navigate=useNavigate();
 
+  const [image, setImage]=useState(false);
+  const [name, setName]=useState("");
+  const [bio, setBio]=useState("");
+  const [uid, setUid]=useState("");
+  const [prevImage, setPrevImage]=useState("");
+  const {setUserData}=useContext(AppContext)
+
+  const profileUpdate=async(event)=>{
+    event.preventDefault();
+    try {
+
+      if(!prevImage && !image){
+        toast.error("Upload Profile Image");
+      }
+      const docRef=doc(db,"users",uid);
+
+      if(image){
+        const imgUrl=await upload(image);
+        setPrevImage(imgUrl);
+        await updateDoc(docRef,{
+          avatar:imgUrl,
+          bio:bio,
+          name:name
+        })
+      }
+      else{
+        await updateDoc(docRef,{
+          bio:bio,
+          name:name
+        })
+      }
+      const snap=await getDoc(docRef);
+      setUserData(snap.data());
+      navigate('/chat');
+
+    } catch (error) {
+      console.error(error);
+      toast.error(error.message);
+    }
+  }
+
+  useEffect(()=>{
+    onAuthStateChanged(auth ,async(user)=>{
+      if(user){
+        setUid(user.uid);
+        const docRef=doc(db,"users",user.uid);
+        const docSnap=await getDoc(docRef);
+
+        if(docSnap.data().name){
+          setName(docSnap.data().name);
+        }
+
+        if(docSnap.data().bio){
+          setBio(docSnap.data().bio);
+        }
+
+        if(docSnap.data().avatar){
+          setPrevImage(docSnap.data().avatar);
+        }
+      }
+      else{
+        navigate('/');
+      }
+    })
+  },[])
   return (
     <div className='profile'>
       <div className="profile-container">
-        <form action="">
+        <form onSubmit={profileUpdate}>
           <h3>Profile Details</h3>
+
           <label htmlFor="avatar">
             <input type="file" onChange={(e)=>setImage(e.target.files[0])} id="avatar" accept=".png, .jpg, .jpeg" hidden />
             <img src={image? URL.createObjectURL(image) : assets.avatar_icon} alt="" />
             Upload Profile Image
           </label>
-          <input type="text" placeholder='Your name' required />
-          <textarea name="" id="" placeholder='Write profile bio'></textarea>
+
+          <input onChange={(e)=>setName(e.target.value)} value={name} type="text" placeholder='Your name' required />
+          <textarea onChange={(e)=>setBio(e.target.value)} value={bio} name="" id="" placeholder='Write profile bio'></textarea>
           <button type="submit">Save</button>
+
         </form>
-        <img className='profile-pic' src={image? URL.createObjectURL(image) : assets.logo_icon} alt="" />
+
+        <img className='profile-pic' src={image? URL.createObjectURL(image) : prevImage ? prevImage : assets.logo_icon} alt="" />
       </div>
     </div>
   )
